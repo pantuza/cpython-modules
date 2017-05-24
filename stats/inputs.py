2c51b7d4fc3ad2fcba8ba60ac953403683e826de
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-05-23 22:59:03 -0300

    Stats module: Create function to build lists
    This functions creates a random data set with sizes at most MAX_LEN

---
+++ stats/inputs.py
@@ -0,0 +1,26 @@
+from random import randint
+
+
+MAX_LEN = 100000
+
+
+def build_list(n_items):
+    """ Creates a randomized list with size n_items """
+
+    rand_list = []
+    i = 0
+
+    while i < n_items:
+        rand_list.append(randint(0, n_items))
+        i += 1
+
+    return rand_list
+
+#
+# Random data set
+#
+inputs = []
+
+for i in range(0, MAX_LEN):
+
+    inputs.append(build_list(randint(1, MAX_LEN)))
