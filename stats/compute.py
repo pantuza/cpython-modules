fa1777f6f94748808d0eeb5da4e2572d61c15470
Gustavo Pantuza <gustavopantuza@gmail.com>
2017-05-24 11:46:59 -0300

    Experiments: Create method for compute mean times

--- stats/compute.py
+++ stats/compute.py
@@ -12,6 +12,24 @@ logs = {
     "median": "median.log"
 }
 
+def compute_mean(input_list):
+   """ Compute time execution of each module """
+
+    begin_py_mean = clock()
+    py_mean(input_list)
+    end_py_mean = clock()
+
+    begin_mean = clock()
+    mean(input_list)
+    end_mean = clock()
+
+    with open(logs['mean'], "a") as file:
+        file.write("%f %f\n" % (
+                abs(end_py_mean - begin_py_mean),
+                abs(begin_mean - end_mean)
+            )
+        )
+
 def compute():
     """ Function to run computation experiment """
 
@@ -21,21 +39,8 @@ def compute():
     try:
 
         for input_list in inputs:
+            compute_mean(input_list)
 
-            begin_py_mean = clock()
-            py_mean(input_list)
-            end_py_mean = clock()
-
-            begin_mean = clock()
-            mean(input_list)
-            end_mean = clock()
-
-            with open(logs['mean'], "a") as file:
-                file.write("%f %f\n" % (
-                        abs(end_py_mean - begin_py_mean),
-                        abs(begin_mean - end_mean)
-                    )
-                )
     except Exception as e:
         raise e
 
