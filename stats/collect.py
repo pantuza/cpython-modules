b441aac44229606219427a757e3942062e09dac6
Gustavo Pantuza <gustavopantuza@gmail.com>
2017-05-24 14:56:12 -0300

    Experiments: Refactor all compute names to collect

--- stats/collect.py
+++ stats/collect.py
@@ -34,7 +34,7 @@ def clear_logs():
 
 
 def save_times(times, file_name):
-    """ Writes the computed times in given file """
+    """ Writes the collected times in given file """
 
     with open(file_name, "a") as file:
         file.write("%s\n" % times)
@@ -43,13 +43,13 @@ def save_times(times, file_name):
 
 
 def format_times(t0, t1):
-    """ Formats the computed times """
+    """ Formats the collected times """
 
     return "%.2f %.2f" % (t0 * MICROSECS, t1 * MICROSECS)
 
 
-def compute_mean(input_list):
-    """ Compute time execution of mean of each module """
+def collect_mean(input_list):
+    """ Collect time execution of mean of each module """
 
     begin_py_mean = clock()
     py_mean(input_list)
@@ -63,8 +63,8 @@ def compute_mean(input_list):
     save_times(times, logs['mean'])
 
 
-def compute_mode(input_list):
-    """ Compute time execution of mode of each module """
+def collect_mode(input_list):
+    """ Collect time execution of mode of each module """
 
     begin_py_mode = clock()
     py_mode(input_list)
@@ -78,8 +78,8 @@ def compute_mode(input_list):
     save_times(times, logs['mode'])
 
 
-def compute_median(input_list):
-    """ Compute time execution of median of each module """
+def collect_median(input_list):
+    """ Collect time execution of median of each module """
 
     begin_py_median = clock()
     py_median(input_list)
@@ -94,7 +94,7 @@ def compute_median(input_list):
     save_times(times, logs['median'])
 
 
-def compute():
+def collect():
     """ Function to run computation experiment """
 
     print("Starting computation..")
@@ -105,9 +105,9 @@ def compute():
 
         for i, input_list in enumerate(inputs):
             print("input[%d]" % i)
-            compute_mean(input_list)
-            compute_mode(input_list)
-            compute_median(input_list)
+            collect_mean(input_list)
+            collect_mode(input_list)
+            collect_median(input_list)
 
     except Exception as e:
         raise e
@@ -121,4 +121,4 @@ def compute():
 if __name__ == "__main__":
 
     clear_logs()
-    compute()
+    collect()
