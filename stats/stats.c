d825ed9372221ae2bdd0399b3c6545d6ebd07919
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-05-23 22:53:16 -0300

    Stats module: Create function to calculate mean

--- stats/stats.c
+++ stats/stats.c
@@ -19,10 +19,33 @@
 #include <Python.h>
 
 
+/**
+ * Calculates the mean of a given list of integers
+ */
 static PyObject *
 mean (PyObject *self, PyObject *args)
 {
-    Py_RETURN_NONE;
+    PyObject* list_obj;
+    PyObject* sequence;
+    PyObject* item;
+
+    if (!PyArg_ParseTuple(args, "O", &list_obj)) {
+        return NULL;
+    }
+
+    sequence = PySequence_Fast(list_obj, "Expected a list object");
+    int seq_size = PySequence_Size(list_obj);
+
+    long sum = -1;
+    for(int i = 0; i < seq_size; i++) {
+
+        item = PySequence_Fast_GET_ITEM(sequence, i);
+        sum += _PyLong_AsInt(item);
+    }
+
+    Py_DECREF(sequence);
+
+    return Py_BuildValue("i", sum / seq_size);
 }
 
 
@@ -51,7 +74,7 @@ static struct PyModuleDef statsmodule = {
     PyModuleDef_HEAD_INIT,
     "stats",    /* name of module */
     "Statistics module for Python written in C",  /* doc string, may be NULL */
-    -1, /* size of per-interpreter state of the module,
+    -1, /* size of per-interpreter state of the module, 
            or -1 if the module keeps state in global variables */
     module_methods   /* methods table */
 };
