c95b38722e305fe2975b2f51bc7a9231ff356fb6
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-05-23 23:01:28 -0300

    Stats module: Create script to compare modules

---
+++ stats/compute.py
@@ -0,0 +1,42 @@
+import gc
+from time import clock
+from statistics import mean as py_mean
+
+from stats import mean
+from inputs import inputs
+
+
+logs = {
+    "mean": "mean.log",
+    "mode": "mode.log",
+    "median": "median.log"
+}
+
+
+gc_ref = gc.isenabled()
+gc.disable()
+
+try:
+
+    for input_list in inputs:
+
+        begin_py_mean = clock()
+        py_mean(input_list)
+        end_py_mean = clock()
+
+        begin_mean = clock()
+        mean(input_list)
+        end_mean = clock()
+
+        with open(logs['mean'], "a") as file:
+            file.write("%f %f\n" % (
+                    abs(end_py_mean - begin_py_mean),
+                    abs(begin_mean - end_mean)
+                )
+            )
+except Exception as e:
+    raise e
+
+finally:
+    if gc_ref:
+        gc.enable()
