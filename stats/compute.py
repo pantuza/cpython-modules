30827a83615e3621da44ffd86c3608f8f1a625f7
Gustavo Pantuza <gustavopantuza@gmail.com>
2017-05-24 11:43:44 -0300

    Experiments: Create separated function compute

--- stats/compute.py
+++ stats/compute.py
@@ -12,31 +12,38 @@ logs = {
     "median": "median.log"
 }
 
+def compute():
+    """ Function to run computation experiment """
 
-gc_ref = gc.isenabled()
-gc.disable()
+    gc_ref = gc.isenabled()
+    gc.disable()
 
-try:
+    try:
 
-    for input_list in inputs:
+        for input_list in inputs:
 
-        begin_py_mean = clock()
-        py_mean(input_list)
-        end_py_mean = clock()
+            begin_py_mean = clock()
+            py_mean(input_list)
+            end_py_mean = clock()
 
-        begin_mean = clock()
-        mean(input_list)
-        end_mean = clock()
+            begin_mean = clock()
+            mean(input_list)
+            end_mean = clock()
 
-        with open(logs['mean'], "a") as file:
-            file.write("%f %f\n" % (
-                    abs(end_py_mean - begin_py_mean),
-                    abs(begin_mean - end_mean)
+            with open(logs['mean'], "a") as file:
+                file.write("%f %f\n" % (
+                        abs(end_py_mean - begin_py_mean),
+                        abs(begin_mean - end_mean)
+                    )
                 )
-            )
-except Exception as e:
-    raise e
+    except Exception as e:
+        raise e
 
-finally:
-    if gc_ref:
-        gc.enable()
+    finally:
+        if gc_ref:
+            gc.enable()
+
+
+if __name__ == "__main__":
+
+    compute()
