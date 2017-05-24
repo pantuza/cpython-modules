5cab7541611586087733480a9271cd18e8457a46
Gustavo Pantuza <gustavopantuza@gmail.com>
2017-05-24 12:02:24 -0300

    Experiments: Create function to compute mode times

--- stats/compute.py
+++ stats/compute.py
@@ -2,8 +2,13 @@ import gc
 from sys import stdout
 from time import clock
 from statistics import mean as py_mean
+from statistics import mean as py_mode
+from statistics import mean as py_median
 
 from stats import mean
+from stats import mode
+from stats import median
+
 from inputs import inputs
 
 
@@ -21,13 +26,15 @@ def save_times(times, file_name):
 
     print >> stdout, times
 
+
 def format_times(t0, t1):
     """ Formats the computed times """
 
-    return "%f %f\n" % (abs(t0), abs(t1))
+    return "%f %f\n" % (t0, t1)
+
 
 def compute_mean(input_list):
-   """ Compute time execution of each module """
+    """ Compute time execution of mean of each module """
 
     begin_py_mean = clock()
     py_mean(input_list)
@@ -37,8 +44,24 @@ def compute_mean(input_list):
     mean(input_list)
     end_mean = clock()
 
-    times = format_times(end_py_mean - begin_py_mean, begin_mean - end_mean)
+    times = format_times(end_py_mean - begin_py_mean, end_mean - begin_mean)
     save_times(times, logs['mean'])
+
+
+def compute_mode(input_list):
+    """ Compute time execution of mode of each module """
+
+    begin_py_mode = clock()
+    py_mode(input_list)
+    end_py_mode = clock()
+
+    begin_mode = clock()
+    mode(input_list)
+    end_mode = clock()
+
+    times = format_times(end_py_mode - begin_py_mode, end_mode - begin_mode)
+    save_times(times, logs['mean'])
+
 
 def compute():
     """ Function to run computation experiment """
@@ -50,6 +73,7 @@ def compute():
 
         for input_list in inputs:
             compute_mean(input_list)
+            compute_mode(input_list)
 
     except Exception as e:
         raise e
