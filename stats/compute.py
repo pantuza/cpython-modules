9402105b85cbb2d682c5c1c16cf93fa27065c398
Gustavo Pantuza <gustavopantuza@gmail.com>
2017-05-24 11:58:17 -0300

    Experiments: Create function to format times

--- stats/compute.py
+++ stats/compute.py
@@ -21,6 +21,10 @@ def save_times(times, file_name):
 
     print >> stdout, times
 
+def format_times(t0, t1):
+    """ Formats the computed times """
+
+    return "%f %f\n" % (abs(t0), abs(t1))
 
 def compute_mean(input_list):
    """ Compute time execution of each module """
@@ -33,11 +37,7 @@ def compute_mean(input_list):
     mean(input_list)
     end_mean = clock()
 
-    times = "%f %f\n" % (
-        abs(end_py_mean - begin_py_mean),
-        abs(begin_mean - end_mean)
-    )
-
+    times = format_times(end_py_mean - begin_py_mean, begin_mean - end_mean)
     save_times(times, logs['mean'])
 
 def compute():
